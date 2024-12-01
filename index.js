710368f1883f4ad2ef6c506c641e9f5d2227b1da
Aman Rathod <amanrathod07.ar@gmail.com>
2024-12-01 11:49:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter' , (req , res) => {
+    res.send("Chalu ho gya")
+})
+
+app.get('/aman' , (req , res) => {
+    res.send("cookie");
+})
+
+app.listen(process.env.port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
